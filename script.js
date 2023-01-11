f8e53e91445958180e642c47d8d6f831fb9f4451
xbiotech <biotech9261@gmail.com>
2023-01-11 15:33:16 +0100

    Add function for player to select a game move and to declare winner, loser or if it is a tie

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const getRandomName = ["Rock", "Paper", "Scissor"];
 
-function getComputerChoice(getRandomName){
+function getComputerChoice(){
     return getRandomName [Math.floor(Math.random()* getRandomName.length)];
 }
-console.log(getComputerChoice(getRandomName));
+
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie"
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        return "You Won"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return "You Won"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
+        return "You Lose"
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
+        return "You Lose"
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        return "You Won"
+    } else return "Click F5 to refresh the Page and enter a valid game move"
+  }
+
+
+const playerSelection = prompt("Choose your Weapon");
+const computerSelection = getComputerChoice();
+
+
+
+console.log(playRound(playerSelection, computerSelection));
